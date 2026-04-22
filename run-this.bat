3bc0f96d8ddc218d21fc138b3e7f0e7b5eef19cd
employee1645 <employee1645@gmail.com>
2026-04-22 21:28:42 +0300

    Update and rename run-this.bat to run-in-directory.bat

--- run-this.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell.exe -Command "cd $env:TEMP; Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/employee1645/pfp-crisp/main/SetProfilePic.ps1' -OutFile 'SetProfilePic.ps1'; powershell.exe -ExecutionPolicy Bypass -File 'SetProfilePic.ps1'"
-pause
