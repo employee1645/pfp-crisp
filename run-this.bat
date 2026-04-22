632907634852457d07e645945bc3f25a30df28fb
employee1645 <employee1645@gmail.com>
2026-04-22 21:21:10 +0300

    Update run-this.bat

--- run-this.bat
+++ run-this.bat
@@ -1,2 +1,3 @@
 @echo off
-powershell.exe -Command "cd $env:TEMP; Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/employee1645/pfp-crisp/main/SetProfilePic.ps1' -OutFile 'SetProfilePic.ps1'; powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File 'SetProfilePic.ps1'"
+powershell.exe -Command "cd $env:TEMP; Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/youruser/yourrepo/main/SetProfilePic.ps1' -OutFile 'SetProfilePic.ps1'; powershell.exe -ExecutionPolicy Bypass -File 'SetProfilePic.ps1'"
+pause
